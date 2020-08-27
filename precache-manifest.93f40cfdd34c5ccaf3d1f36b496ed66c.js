eda32c2c7c1cbd9e4401ec97a0b854b24a7b2e5b
vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2020-08-27 15:18:56 +0530

    Updates

---
+++ precache-manifest.93f40cfdd34c5ccaf3d1f36b496ed66c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0697d6e64329ff8e08f2160fe8006c94",
+    "url": "/decide2.0/index.html"
+  },
+  {
+    "revision": "edbe7149da4ec0f8cdfd",
+    "url": "/decide2.0/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fe75c65cbbdb9361c97",
+    "url": "/decide2.0/static/js/2.b0b689a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/decide2.0/static/js/2.b0b689a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edbe7149da4ec0f8cdfd",
+    "url": "/decide2.0/static/js/main.28bb7747.chunk.js"
+  },
+  {
+    "revision": "961541cc505c3283312d",
+    "url": "/decide2.0/static/js/runtime-main.52808e15.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/decide2.0/static/media/logo.5d5d9eef.svg"
+  }
+]);
